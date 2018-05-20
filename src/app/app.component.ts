f8b589935cf48b30e7e251fe03ad041af8790922
shyam <shyamkishor28@gmail.com>
2018-05-20 17:19:47 +0530

    this is my first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  weekdays=['Mon','Tue','Wed'];
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { MyfirstcomponentComponent } from './myfirstcomponent/myfirstcomponent.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyfirstcomponentComponent
   ],
   imports: [
     BrowserModule
